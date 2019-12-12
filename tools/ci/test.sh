a774de55795b2ff8aa2ff95c1ee069a6d054c502
B. van Berkum <dev@dotmpe.com>
2019-12-12 21:17:55 +0100

    Testing

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -2,10 +2,15 @@
 
 git clone http://github.com/dotmpe/docker-arduino ~/build/dotmpe/docker-arduino
 
-echo $PATH
-
 ln -s ~/build/dotmpe/docker-arduino/exec.sh ~/bin/arduino
 
 arduino core install arduino:avr
 
 arduino compile -b arduino:avr:pro Prototype/Blink
+arduino compile -b arduino:avr:pro Prototype/Node
+arduino compile -b arduino:avr:pro Prototype/AtmegaEEPROM
+
+for x in Prototype/*/
+do
+  arduino compile -b arduino:avr:pro $x || true
+done
